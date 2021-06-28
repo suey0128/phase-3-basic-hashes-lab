e5a318597cfc06c06423bba398c3095ba3a9867c
suey0128 <syui0128@gmail.com>
2021-06-28 13:56:20 -0700

    commit tree 80499f248cd600702b89d142df0c7f1b9d3a31f5

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,17 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  this_hash={aaa: "bbb"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  this_pioneer = {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  id_hash = {id: number}
 end
